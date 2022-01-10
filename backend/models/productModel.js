8a52129f645a2259bd1026c47f5af65922c822d7
Chirag Jain <cjain8060@gmail.com>
2022-01-10 23:51:29 +0530

    created data models for user,product and order

---
+++ backend/models/productModel.js
@@ -0,0 +1,80 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  // this is the individual review rating and not the average rating
+  rating: {
+    type: Number,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+});
+
+const productSchema = new Schema(
+  {
+    user: {
+      // to know which user(admin) created the product
+      type: Schema.Types.ObjectId,
+      required: true,
+      // adds a realtion b/w the user and product models
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    // will have different review documents inside it
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
