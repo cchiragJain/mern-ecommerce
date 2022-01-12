2a0904e37bf573780ada43b1544dc604c0764298
Chirag Jain <cjain8060@gmail.com>
2022-01-12 18:59:37 +0530

    changed productRoutes to use controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,39 +1,16 @@
 import express from "express";
-// using asyncHandler so no need to use try-catch blocks
-import asyncHandler from "express-async-handler";
 
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // setTimeout(() => {}, 1000);
-    res.json(products);
-  })
-);
+// using .route instead of router.get('/',controller)
+// can add different methods to the same route then without explicitly mentioning the path
+router.route("/").get(getProducts);
 
-// @desc Fetch single products
-// @route GET /api/products/:id
-// @access Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.route("/:id").get(getProductById);
 
 export default router;
