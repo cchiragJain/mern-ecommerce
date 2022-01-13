83d32b000f0a256091102c793dd906ff6b93700f
Chirag Jain <cjain8060@gmail.com>
2022-01-13 21:38:36 +0530

    added auth middleware and authorized profile api route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/login").post(authUser);
+
+// protect middleware will run when we access that route
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
